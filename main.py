0e227bb84991f8fa67ae0012562b31f1b632ef34
omer <omeru0663@gmail.com>
2023-11-24 11:59:33 +0000

    improved code

--- main.py
+++ main.py
@@ -1,4 +1,23 @@
 import datetime
 currentyear = datetime.datetime.now().year
-userinput = int(input("Enter Year Of Birth: "))
-print(f"you are {currentyear - userinput} years old")
+currentmonth = datetime.datetime.now().month 
+currentday = datetime.datetime.now().day
+userinput = input("Enter Year Of Birth (dd/mm/yyyy): ")
+
+day, month, year = userinput.split("/")
+dayborn = int(day)
+monthborn = int(month)
+yearborn = int(year)
+if yearborn < 100 and yearborn > 24:
+    yearborn = yearborn + 1900
+elif yearborn <= 23:
+    yearborn = yearborn + 2000
+else:
+    yearborn = yearborn + 0
+ 
+if dayborn - currentday <= 0 and monthborn - currentmonth <= 0:
+    age = currentyear -  yearborn
+else: 
+    age = currentyear - yearborn - 1 
+
+print(f"you are {age} years old")
