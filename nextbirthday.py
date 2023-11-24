2d25719065c752abf5430a12b3114b3857828493
omer <omeru0663@gmail.com>
2023-11-24 12:04:21 +0000

    minor improvements

--- nextbirthday.py
+++ nextbirthday.py
@@ -2,12 +2,11 @@ import datetime as dt
 userinput = input("Enter Date Of Birth (dd/mm/yyyy): ")
 daysineachmonth = [31,28,31,30,31,30,31,31,30,31,30,31]
 totaldays = 0 
-day, month, year = userinput.split("/")
+day, month, _ = userinput.split("/")
 
-monthindex = int(month) 
+monthindex = int(month) - 1 
 while monthindex > -1:
     totaldays = totaldays + daysineachmonth[monthindex]
     monthindex = monthindex - 1 
-
 print(f"days until yout birthday: {totaldays}")
 
