4f41480d099497769c32aa83af7cf943a3d94726
omer <omeru0663@gmail.com>
2023-11-24 11:00:04 +0000

    imported datetime

--- main.py
+++ main.py
@@ -1,2 +1,4 @@
-userinput = input("Enter Year Of Birth: ")
-print(f"you are  {userinput} years old")
+import datetime
+currentyear = datetime.datetime.now().year
+userinput = int(input("Enter Year Of Birth: "))
+print(f"you are {currentyear - userinput} years old")
